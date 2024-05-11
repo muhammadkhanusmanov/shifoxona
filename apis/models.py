9053db17dd9941f8beb3401d3ea0cae9fbd07dc3
username <email>
2024-05-11 15:29:59 +0300

    models

--- apis/models.py
+++ apis/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Branch(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField()
+    desc = models.CharField(max_length=200)
+    desc2 = models.CharField(max_length=100,blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField()
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE,related_name='Doctor')
+    desc = models.CharField(max_length=200)
+    desc2 = models.CharField(max_length=100,blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+    
