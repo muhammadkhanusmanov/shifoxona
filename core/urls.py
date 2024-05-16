84c7f7aa55b7a4cda2131eba5e4dd9d046459de5
username <email>
2024-05-16 20:36:29 +0300

    .

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-from ..apis.views import (BranchImg,DoctorImg,
+from apis.views import (BranchImg,DoctorImg,
    DoctorView,Resption,PatientsView
 )
 
