a8a9ac8f630db723a4762178e52dd323d9157a6b
username <email>
2024-05-16 08:54:29 +0300

    serial

--- apis/serialazers.py
+++ apis/serialazers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField, ReadOnlyField
-from .models import Doctor,Branch
+from .models import Doctor,Branch,Patients
 from django.contrib.auth.models import User
 
 class DoctorSerializer(ModelSerializer):
@@ -10,4 +10,9 @@ class DoctorSerializer(ModelSerializer):
 class BranchSerializer(ModelSerializer):
     class Meta:
         model = Branch
+        fields = '__all__'
+
+class PatientsSerializer(ModelSerializer):
+    class Meta:
+        model = Patients
         fields = '__all__'
