a76aa720585ec154fc98eff1ee0dce0700255270
username <email>
2024-05-16 07:23:19 +0300

    register

--- apis/admin.py
+++ apis/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import (Doctor,Branch)
+from .models import (Doctor,Branch,Patients)
 
-admin.site.register([Doctor,Branch])
+admin.site.register([Doctor,Branch,Patients])
+
